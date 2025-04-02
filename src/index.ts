8666283c31038a53aee4ca3c5f1831c2f0d7e0e9
dangars151 <dangars151@gmail.com>
2025-04-03 00:40:10 +0700

    add sign in sign up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
 import { AppDataSource } from "./data-source"; 
 import { User } from "./entity/User";
+import dotenv from "dotenv";
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -10,7 +15,59 @@ const PORT = 3000;
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("🔗 Kết nối đến database thành công!");
+    console.log("🔗 Connect database successfully!");
+
+    app.post("/signup", async (req, res) => {
+      const { email, password } = req.body;
+
+      const user = await AppDataSource.getRepository(User).findOne({
+        where: { email },
+      });
+    
+      if (user) {
+        res.status(400).json({ message: "User exist!" });
+        return 
+      }
+      
+      const hashedPassword = await bcrypt.hash(password, 10);
+    
+      const newUser: User = {
+        email,
+        password: hashedPassword,
+      };
+
+      await AppDataSource.getRepository(User).save(newUser);
+    
+      res.status(201).json({ message: "Đăng ký thành công!" });
+    });
+
+    app.post("/signin", async (req, res) => {
+      const { email, password } = req.body;
+    
+      const user = await AppDataSource.getRepository(User).findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        res.status(404).json({ message: "User not found!" });
+        return 
+      }
+    
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        res.status(400).json({ message: "Incorrect password!" });
+        return
+      }
+    
+      const secretKey = process.env.JWT_SECRET;
+      if (!secretKey) {
+        throw new Error('JWT_SECRET is not defined in the environment variables');
+      }
+
+      const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '24h' });
+    
+      res.status(200).json({ token });
+    });
 
     app.get("/users", async (req, res) => {
       const users = await AppDataSource.getRepository(User).find();
